95c4e2830a758bc57a99019c927361407016da42
madiha8902 <madiha.batool@bellfield.nsw.edu.au>
2024-10-03 19:20:34 +1000

    homework into loops

---
+++ main.py
@@ -0,0 +1,9 @@
+# str="Madiha"
+# for i in str:
+#     print(i)
+
+# for i in range(0,100,2):
+#     print(i)
+# number = int(input("enter number for table"))
+# for i in range(0,100):
+#     print(number,"x",i,"=",number * i)
